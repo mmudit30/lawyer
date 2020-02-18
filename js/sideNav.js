86c5a6492e0291e47f210a519c895b0fed144572
Mudit Mittal <mmudit30@gmail.com>
2020-02-18 19:22:40 +0530

    false commit

--- js/sideNav.js
+++
@@ -1,9 +0,0 @@
-function openNav() {
-    document.getElementById("mySidenav").style.width = "250px";
-    document.getElementById("sideNavMenu").style.marginLeft = "250px";
-  }
-  
-  function closeNav() {
-    document.getElementById("mySidenav").style.width = "0";
-    document.getElementById("sideNavMenu").style.marginLeft= "0";
-  }
